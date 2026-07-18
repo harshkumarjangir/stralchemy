c6e058469244f058e3e7cc5fc2a60d213987a8f8
Harsh Kumar Jangir <harshkumarjangir2002@gmail.com>
2026-07-18 19:59:58 +0530

    feat: initialize express server with database connection and middleware configuration

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import routes from './routes/index.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5100;
+const PORT = process.env.PORT || 6000;
 
 import path from 'path';
 
